59ef1d27612bb601fecbe74d8d5d46685bd45c06
mmihye <smilehye910@naver.com>
2025-08-06 03:05:57 +0900

    [#1] fix : internal-api 통합

--- toaster-api/src/main/java/com/app/toaster/ToasterApiApplication.java
+++ toaster-api/src/main/java/com/app/toaster/ToasterApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToasterExternalApiApplication {
+public class ToasterApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ToasterExternalApiApplication.class, args);
+        SpringApplication.run(ToasterApiApplication.class, args);
     }
 }

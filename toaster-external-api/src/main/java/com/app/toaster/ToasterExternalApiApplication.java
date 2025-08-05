84d39e694e0d271617d953221eb591c9adb37b69
mmihye <smilehye910@naver.com>
2025-08-06 02:28:57 +0900

    [#1] feat : DB 연결

---
+++ toaster-external-api/src/main/java/com/app/toaster/ToasterExternalApiApplication.java
@@ -0,0 +1,11 @@
+package com.app.toaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToasterExternalApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToasterExternalApiApplication.class, args);
+    }
+}

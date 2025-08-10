c288e0c0c89b726b3a809d58bc1ba9ff6c9a75b0
soohyun <sss4920@naver.com>
2025-08-10 19:36:06 +0900

    feat: clipEntity 공유클립 관련 로직 삽입.

--- toaster-api/src/main/java/com/app/toaster/adapter/out/persistence/clip/ClipEntity.java
+++ toaster-api/src/main/java/com/app/toaster/adapter/out/persistence/clip/ClipEntity.java
@@ -1,19 +1,24 @@
 package com.app.toaster.adapter.out.persistence.clip;
 
+import com.app.toaster.exception.Error;
+import com.app.toaster.exception.model.BadRequestException;
+import com.app.toaster.exception.model.CustomException;
 import com.app.toaster.toast.enums.ClipType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "clip")
 class ClipEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -31,13 +36,77 @@ class ClipEntity {
 
 	private String members;
 
-	private LocalDateTime latestReadTime;
 	@Builder
-	public ClipEntity(String title, Long ownerId, int priority, LocalDateTime latestReadTime) {
+	public ClipEntity(String title, Long ownerId, int priority, ClipType clipType) {
 		this.title = title;
 		this.ownerId = ownerId;
 		this.members = null;
 		this.priority = priority;
-		this.latestReadTime = latestReadTime;
+		this.type = clipType;
 	}
+
+	public void changePrivate() {
+		if (this.type.equals(ClipType.PRIVATE) && !isEmptyMemberList()) { //주인만 있지않은 멤버 리스트는 private으로 못만들게
+			throw new BadRequestException(Error.BAD_REQUEST_VISIBILITY_CLIP, Error.BAD_REQUEST_VISIBILITY_CLIP.getMessage());
+		}
+		this.type = ClipType.PRIVATE;
+		this.members = makeOnlyOwnerState();
+	}
+
+	public void changeShared() {
+		if (this.type.equals(ClipType.SHARED)) {
+			throw new BadRequestException(Error.BAD_REQUEST_VISIBILITY_CLIP, Error.BAD_REQUEST_VISIBILITY_CLIP.getMessage());
+		}
+		this.type = ClipType.SHARED;
+		this.members = makeOnlyOwnerState();
+	}
+
+	public void addMember(Long newMemberId) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			List<Long> memberList = objectMapper.convertValue(this.members, List.class);
+			memberList.add(newMemberId);
+			this.members = objectMapper.writeValueAsString(memberList);
+		} catch (Exception e) {
+			throw new CustomException(Error.UNPROCESSABLE_ENTITY_CONVERT_EXCEPTION, Error.UNPROCESSABLE_ENTITY_CONVERT_EXCEPTION.getMessage());
+		}
+	}
+
+	public void exitMember(Long exitMemberId) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			List<Long> memberList = objectMapper.convertValue(this.members, List.class);
+			memberList.remove(exitMemberId); //TODO: object로 하긴했는데, 이거 인덱스로 오인하고 지우진않겠지?
+			this.members = objectMapper.writeValueAsString(memberList);
+		} catch (Exception e) {
+			throw new CustomException(Error.UNPROCESSABLE_ENTITY_CONVERT_EXCEPTION, Error.UNPROCESSABLE_ENTITY_CONVERT_EXCEPTION.getMessage());
+		}
+	}
+
+	private String makeOnlyOwnerState() {
+		List<Long> list = List.of(ownerId);
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			return objectMapper.writeValueAsString(list);
+		} catch (Exception e) {
+			throw new CustomException(Error.UNPROCESSABLE_ENTITY_CONVERT_EXCEPTION, Error.UNPROCESSABLE_ENTITY_CONVERT_EXCEPTION.getMessage());
+		}
+	}
+
+	private boolean isEmptyMemberList() {
+		if (members.equals(makeOnlyOwnerState())) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * TODO: 생각해볼점.
+	 * 공유클립을 생성했을 때 기존 클립로직은 다 영향 받을듯.
+	 * 1. 해당 클립 수정 -> 기존 클립 수정 api등도 고려해야함. -> 수정 이벤트 히스토리 테이블에 저장. 참여자와 소유자 모두 수정가능.
+	 * 2. 해당 클립 삭제 -> 만약 참가자들이 존재할경우 -> 삭제할수없다. 참가자들이 없을경우 삭제. 이때 softdelete가 목표힘들면 기존 로직 냅두기. 소유자가 아닌사람은 삭제권한 x
+	 * 3. 클립생성 -> 기존 클립을 생성 + 공유클립테이블에도 생성.
+	 * 4. 클립조회 -> 공유클립 조회할 때 만약 내가 참여한 클립을 띄워야하는데, 이는 인덱스처리할 테이블로 해서 기록해야될듯. 아니면 유저에 공유클립 id리스트를 넣어둘까?
+	 * 5. 클립 나가기 -> 공유클립에서
+	 */
 }

796b7cf63ee3de5ae93f88c80e2b100ba5ba0a49
soohyun <sss4920@naver.com>
2025-08-28 00:05:11 +0900

    refactor: 논의 결과 어댑터쪽 transactional 제거

--- toaster-api/src/main/java/com/app/toaster/adapter/out/persistence/clip/ClipPersistenceAdapter.java
+++ toaster-api/src/main/java/com/app/toaster/adapter/out/persistence/clip/ClipPersistenceAdapter.java
@@ -6,7 +6,6 @@ import com.app.toaster.exception.Error;
 import com.app.toaster.exception.model.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -15,13 +14,11 @@ public class ClipPersistenceAdapter implements CheckClipOwnerPort, CheckClipMemb
     private final ClipRepository clipRepository;
 
     @Override
-    @Transactional(readOnly = true)
     public boolean existsByIdAndUserId(Long clipId, Long userId) {
         return clipRepository.existsByIdAndOwnerId(clipId, userId);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public boolean checkUserInClipMember(Long userId, Long clipId) {
         ClipEntity clipEntity = clipRepository.findById(clipId).orElseThrow(
             () -> new CustomException(Error.NOT_FOUND_CLIP_EXCEPTION, Error.NOT_FOUND_CLIP_EXCEPTION.getMessage())
